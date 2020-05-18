8e58f4da1f49f6d5475922bbd11ddf4fce9a82a7
lrhernandes <larachernandes@gmail.com>
2020-05-18 01:14:30 -0300

    fix(navagação no register): agora não temos um scroll

--- frontend/src/templates/CadastrarClientsForm/index.js
+++ frontend/src/templates/CadastrarClientsForm/index.js
@@ -1,72 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css'
 import '../../js/select'
 import {Link} from 'react-router-dom';
 
 export default function CadastrarClientForm(){
+    const [step, setStep] = useState(0);
     return (
         <div>
-            <form id="form-register-scroll">
-                <div id="space-scroll-div">
-                    <h2>Cadastre-se</h2>
-                    <h4>Dados pessoais</h4>
-                        <label>Nome</label><br/>
-                        <input id="firstName" placeholder="Nome"/> 
-        
-                        <label>Sobrenome</label><br/>
-                        <input id="lastName" placeholder="Sobrenome"/>
-
-                        <div className="form-group-1">
-                            <label>RG</label><br/>
-                            <input id="rg" placeholder="Apenas números"/>
-                        </div>
-
-                        <div className="form-group-2">
-                            <label>Data de nascimento</label><br/>
-                            <input id="born" type="date"/>
-                        </div>
+            {step == 0 && (
+                <div>
+                    <form>
+                        <div>
+                            <h2>Cadastre-se</h2>
+                            <h4 className="h4">Dados pessoais</h4>
+                            <label>Nome</label><br/>
+                            <input id="firstName" placeholder="Nome"/> 
                     
-                    <h4 className="space-h4">Endereço</h4>
-                        <label>UF</label><br/>
-                        <select id="uf"><option defaultValue >Estado</option></select>
+                            <label>Sobrenome</label><br/>
+                            <input id="lastName" placeholder="Sobrenome"/>
 
-                        <label>Cidade</label><br/>
-                        <select id="cidade"> <option defaultValue >Cidade</option></select>
+                            <div className="form-group-1">
+                                <label>RG</label><br/>
+                                <input id="rg" placeholder="Apenas números"/>
+                            </div>
 
-                        <label>Logradouro</label><br/>
-                        <input id="street" placeholder="Logradouro"/>
-
-                        <label>Nº</label><br/>
-                        <input id="number" placeholder="Nº"/>
-
-                    
-                    <h4 className="space-h4">Contato</h4>
-                        <label>E-mail</label><br/>
-                        <input id="email" placeholder="E-mail"/>
-
-                        <label>WhatsApp</label><br/>
-                        <p className="form-sujest">Com código do país e área</p>
-                        <input id="whatsapp" placeholder="Apenas números"/>
-
-                    <h4 className="space-h4">Acesso</h4>
-                        <label>Nome de usuário</label><br/>
-                        <input id="user" placeholder="Usuário"/>
-
-                        <label>Senha</label><br/>
-                        <p className="form-sujest">Use no mínimo 6 caracteres</p>
-                        <input id="password" placeholder="Senha"/>
-
-                    <div className="button-group-space">
-                        <Link to="/"><button className="purple">CADASTRE-SE</button></Link>
-                        <Link to="/"><button className="negative-purple">CANCELAR</button></Link>
+                            <div className="form-group-2">
+                                <label>Data de nascimento</label><br/>
+                                <input id="born" type="date"/>
+                            </div>
+                        </div>
+                    </form>
+                    <button className="next-button" onClick={() => setStep(step + 1) }>PRÓXIMO 🡢</button>
+                    <div className="bottom-register">
+                        <p className="login-link"> <Link to="/login">Já possuo cadastro</Link></p> 
                     </div>
                 </div>
-            </form>
-            <div className="bottom-register">
-                <p className="login-link"> <Link to="/login">Já possuo cadastro</Link></p> 
-            </div>
+            )}
+
+            {step == 1 && (
+                <div id="register-step1">
+                    <h4 className="h4">Endereço</h4>
+                    <label>UF</label><br/>
+                    <select id="uf"><option defaultValue >Estado</option></select>
+
+                    <label>Cidade</label><br/>
+                    <select id="cidade"> <option defaultValue >Cidade</option></select>
+
+                    <div className="form-group-3">
+                        <label>Logradouro</label><br/>
+                        <input id="street" placeholder="Logradouro"/>
+                    </div>
+
+                    <div className="form-group-4">
+                        <label>Nº</label><br/>
+                        <input id="number" placeholder="Nº"/>
+                    </div>
+                    <button className="previous-button" onClick={() => setStep(step - 1) }>🡠VOLTAR</button>
+                    <button className="next-button" onClick={() => setStep(step + 1) }>PRÓXIMO 🡢</button>
+                </div>
+            )}
+
+            {step ==2 && (
+                <div id="register-step2">
+                    <h4 className="h4">Contato</h4>
+                    <label>E-mail</label><br/>
+                    <input id="email" placeholder="E-mail"/>
+
+                    <label>WhatsApp</label><br/>
+                    <p className="form-sujest">Com código do país e área</p>
+                    <input id="whatsapp" placeholder="Apenas números"/>
+                    <button className="previous-button" onClick={() => setStep(step - 1) }>🡠VOLTAR</button>
+                    <button className="next-button" onClick={() => setStep(step + 1) }>PRÓXIMO 🡢</button>
+                </div>
+            )}
+
+            {step == 3 && (
+                <div id="register-step3">
+                    <h4 className="h4">Acesso</h4>
+                    <label>Nome de usuário</label><br/>
+                    <input id="user" placeholder="Usuário"/>
+
+                    <label>Senha</label><br/>
+                    <p className="form-sujest">Use no mínimo 6 caracteres</p>
+                    <input id="password" placeholder="Senha"/>
+                    <div className="button-group-space">
+                        <Link to="/"><button className="negative-purple">CANCELAR</button></Link>
+                        <Link to="/"><button className="purple">CADASTRE-SE</button></Link>
+                    </div>
+                </div>
+            )}
+
         </div>
     )
-    
 }
-
--- frontend/src/templates/LoginClientsForm/index.js
+++ frontend/src/templates/LoginClientsForm/index.js
@@ -7,20 +7,18 @@ export default function LoginClientForm(){
         <div>
             <form id="form-login">
                 <h2 id="h2">Entrar</h2>
-                <h4 id="h4">Bem-vindo de volta</h4>
+                <h4 className="h4">Bem-vindo de volta</h4>
                 <label>User</label><br/>
                 <input id="user" placeholder="User"/> 
         
                 <label>Senha</label><br/>
                 <input type="password" id="password" placeholder="••••••••••"/>
-
                 <div className="button-group-space">
                     <Link to="/"><button className="purple">ENTRAR</button></Link>
-                    <Link to="/"><button className="negative-purple">CANCELAR</button></Link>
                 </div>
             </form>
             <div className="bottom-register">
-                <p className="login-link"> <Link to="/register">Não possuo cadastro</Link></p> 
+                <p className="register-link"> <Link to="/register">Não possuo cadastro</Link></p> 
             </div>
         </div>
     )
