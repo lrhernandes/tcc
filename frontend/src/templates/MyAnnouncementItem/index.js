0992355161461f87f6c7de4668c2e19cc890a127
lrhernandes <larachernandes@gmail.com>
2020-05-23 22:58:14 -0300

    fix(announcements): link e hover

--- frontend/src/templates/MyAnnouncementItem/index.js
+++ frontend/src/templates/MyAnnouncementItem/index.js
@@ -6,21 +6,21 @@ import {Link} from 'react-router-dom';
 
 export default function MyAnnouncementItem(){
     return (
-        <div className="announcement-item-from-list" id="announcement-item-from-list-my-announcement">
-            <div className="image-announcement-item-from-list">
-                
-            </div>
-            <div className="description-announcement-item-from-list">
-                <p className="description-announcement-item-from-list-name">MALIA</p>
-                <p className="description-announcement-item-from-list-descript">Cachorro</p>
-                <p className="description-announcement-item-from-list-descript">Fêmea</p>
-                <p className="description-announcement-item-from-list-descript">Adulta</p>
+        <Link to="">
+            <div className="announcement-item-from-list" id="announcement-item-from-list-my-announcement">
+                <div className="image-announcement-item-from-list">
+                </div>
+                <div className="description-announcement-item-from-list">
+                    <p className="description-announcement-item-from-list-name">MALIA</p>
+                    <p className="description-announcement-item-from-list-descript">Cachorro</p>
+                    <p className="description-announcement-item-from-list-descript">Fêmea</p>
+                    <p className="description-announcement-item-from-list-descript">Adulta</p>
 
-                <div className="content-favorite-icon-announcement-item-from-list">
-                    <Link to="/" title="Deletar anúncio"><MdDelete size={20} className="favorite-announcement-item-from-list-icon"/></Link>
+                    <div className="content-favorite-icon-announcement-item-from-list">
+                        <Link to="/" title="Deletar anúncio"><MdDelete size={20} className="favorite-announcement-item-from-list-icon"/></Link>
+                    </div>
                 </div>
             </div>
-        </div>
-        
+        </Link>
     )
 }
