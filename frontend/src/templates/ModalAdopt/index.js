54028a6f86b48e820401ce6b81dc344be66ef32d
lrhernandes <larachernandes@gmail.com>
2020-06-06 18:59:54 -0300

    fix(announcement modal): alinhamento do conteúdo dos botões

--- frontend/src/templates/ModalAdopt/index.js
+++ frontend/src/templates/ModalAdopt/index.js
@@ -13,23 +13,22 @@ export default function ModalAdopt(){
                     <p className="modal-adopt-content">Utilize os botões abaixo para entrar em contato com Lara Hernandes.</p>
                     <div className="modal-adopt-contact-wrappper">
                         <div className="modal-adoption-contact">
-                            <MdPhoneIphone className="icon-modal-adoption-contact" size={20}/>
-                            <p>WhatsApp</p>
+                            <p><MdPhoneIphone className="icon-modal-adoption-contact" size={20}/>
+                            WhatsApp</p>
                         </div>
                         <div className="modal-adoption-contact">
-                            <MdMail className="icon-modal-adoption-contact" size={20}/>
-                            <p>E-Mail</p>
+                            <p><MdMail className="icon-modal-adoption-contact" size={20}/>
+                            Email</p>
                         </div>
                     </div>
-
                     <p id="second-title-modal" className="modal-adopt-title"><strong>Não se esqueça:</strong></p>
                     <p className="modal-adopt-subtitle">Faça download do arquivo e o leia com atenção</p>
                     <p className="modal-adopt-content">Ao adotar um pet na plataforma GetPet você concorda com os termos de adoção responsável.</p>
                     <div className="modal-adoption-file">
-                        <MdInsertDriveFile className="icon-modal-adoption-file" size={20}/>
-                        <p>Termo de Adoção</p>
+                        <p><MdInsertDriveFile className="icon-modal-adoption-contact" size={20}/>
+                        Termo de Adoção</p>
                     </div>
-                </div>
+                    </div>
                 <div className="modal-adopt-image-wrapper">
                 </div>
             </div>
