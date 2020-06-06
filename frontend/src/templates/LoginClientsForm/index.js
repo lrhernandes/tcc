4c244cecd7261229f93244123d58e9104f6f1bdd
lrhernandes <larachernandes@gmail.com>
2020-06-06 16:59:40 -0300

    fix (login clients form): alinhamento do link para cadastro

--- frontend/src/templates/LoginClientsForm/index.js
+++ frontend/src/templates/LoginClientsForm/index.js
@@ -4,7 +4,7 @@ import './styles.css';
 
 export default function LoginClientForm(){
     return (
-        <div>
+        <div classname="login-clients-form">
             <form id="form-login">
                 <h2 id="h2">Entrar</h2>
                 <h4 className="h4">Bem-vindo de volta</h4>
