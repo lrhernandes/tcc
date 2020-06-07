49e55d109dd5575e66b2cfa0503f7b225f121aff
lrhernandes <larachernandes@gmail.com>
2020-06-06 22:19:53 -0300

    feat(client settings): adicionando conteúdo

--- frontend/src/templates/ContentClientSettings/index.js
+++ frontend/src/templates/ContentClientSettings/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './styles.css';
 
+import profilepic from '../../images/profile 2.jpg'
+
 export default function ContentClientSettings(){
     return (
         <div className="content-client-settings">
-            
+            <div className="content-client-settings-left-wrapper">
+                <img src={profilepic} className="profile-pic-client-settings"/>
+                <p className="name-client-settings">Lara Cardoso Hernandes</p>
+                <p className="username-client-settings">@lrhernandes</p>
+            </div>
+            <div className="content-client-settings-right-wrapper">
+                AAAA
+            </div>
         </div>
     )
 }
